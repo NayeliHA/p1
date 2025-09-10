942e97d5ed54951075b9a4aed0c7dced7f1ef5ae
NayeliRocio <2411110085@undc.edu.pe>
2025-09-10 16:16:17 -0500

    Inicializando rama main

--- js.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Página cargada 💖");
-
-  const contacto = document.getElementById("contacto");
-  contacto.addEventListener("click", () => {
-    alert("¡Gracias por querer contactarme! 🌸");
-  });
-});
